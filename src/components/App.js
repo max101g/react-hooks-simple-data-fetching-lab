9725daba640dda90b0b26c1a8d5634ca3e11b1fb
max101g <maxgithinji101@gmail.com>
2023-01-13 16:36:01 +0300

    update react

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+
+function App () {
+    const [displayDog, setDisplayDog] = useState (null)
+
+    useEffect (()=> {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+            .then ((response)=> response.json ())
+            .then ((data)=> setDisplayDog(data.message))
+    }, [])
+
+    if (!displayDog) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img 
+                src={displayDog}
+                alt="A Random Dog"
+            />
+        </div>
+    )
+}
+
+export default App
